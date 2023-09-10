c46634e8570e195ef8ecf1b1a86b110a75bdc8e2
ankit gupta <ankitgupta17878@gmail.com>
2023-09-10 19:15:40 +0530

    mongodb connect : date : 9/10/23

--- index.js
+++ index.js
@@ -1,43 +1,13 @@
-          const express=require('express')
-          const app=express();
-         
-           
-          // (req, resp, next): These are the parameters of the reqFilter function:
+const {MongoClient} =require('mongodb');
+const url ='mongodb://0.0.0.0:27017';
 
-          // req: Short for "request," it represents the incoming HTTP request from a client, typically containing information like request headers, query parameters, and request body.
-          // resp: Short for "response," it represents the HTTP response that will be sent back to the client, containing response headers and body.
-          // next: This is a callback function that is used to pass control to the next middleware or route handler in the request-response cycle. Calling next() inside this middleware allows the application to move to the next piece of middleware or route handler.
-          
-         const reqFilter=(req,resp,next)=>{
-          if(!req.query.age){
-              resp.send("please enter age")
-          }
+const  client = new MongoClient(url);
+async function getData(){
+  let result=await client.connect();
+  let db=result.db('e-comm'); 
+  let collection=db.collection('products');
+   let reponse=await collection.find({}).toArray();
+  console.log(reponse);
 
-          else if(req.query.age<17){
-            resp.send("under age")
-          }
-
-          else{console.log();
-            next()
-          }
-
-         }
-
-          app.get('/',reqFilter,(req,res)=>{
-            res.send("this is home")
-
-          })
-
-          app.get('/home',reqFilter,(req,res)=>{
-            res.send("this is home")
-
-          })
-
-          app.get('/contact',(req,res)=>{
-            res.send("this is home")
-
-          })
-
-          app.listen(5050,'localhost',()=>{
-            console.log("success")
-          })
+} 
+getData();
