767cd706fe5f81e1b1957d5f2f367ce93a69c134
ankit gupta <ankitgupta17878@gmail.com>
2023-09-12 14:12:00 +0530

    Dynamic curd Operation using postman with mongodb, date:12/9/23

---
+++ PostmanWithMongo/curdOpr.js
@@ -0,0 +1,47 @@
+const express=require('express');
+require('./config');
+
+
+const product = require('./product');
+
+const app=express();
+app.use(express.json());
+
+// create
+
+app.post('/create', async(req,res)=>{
+    console.log(req.body)
+    let data=new product(req.body);
+    const result= await data.save();
+   res.send("done") 
+});
+
+//Read
+
+app.get('/list', async(req,res)=>{
+  
+    let data=await product.find({});
+   
+   res.send(data) 
+});
+
+
+//delete
+// parms always return in the form of jason
+app.delete('/delete/:_id', async(req,res)=>{
+    console.log(req.params)
+    let data=await product.deleteOne(req.params);
+   
+   res.send(data) 
+});
+
+//update
+app.put('/update/:_id', async(req,res)=>{
+    console.log(req.params)
+    let data=await product.updateOne(req.params,{$set:req.body});
+   
+   res.send(data) 
+});
+
+
+app.listen(5000); 
