7094fcec13a0d2b968d8b31a5ab03d09f6eeef25
ankit gupta <ankitgupta17878@gmail.com>
2023-09-12 17:43:50 +0530

    searching data from database using api, date:12/9/23

---
+++ PostmanWithMongo/SearchingApi.js
@@ -0,0 +1,20 @@
+const express=require('express');
+require('./config');
+const product = require('./product');
+
+const app=express();
+
+app.get("/search/:key", async(req,res)=>{
+console.log(req.params.key)
+    let data= await product.find({
+        "$or":[
+            {"name":{$regex:req.params.key}},
+            {"name":{$regex:req.params.key}}
+        ]
+    });
+ 
+   res.send(data) 
+   console.log(data);
+});
+
+app.listen(5000);
