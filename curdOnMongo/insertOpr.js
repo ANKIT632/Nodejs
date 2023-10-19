1662a943a5b17a70842ba150dc58867b0b26192d
Ankit kumar Gupta <119320392+ANKIT632@users.noreply.github.com>
2023-10-20 00:35:46 +0530

    Update insertOpr.js
    Insert operations

--- curdOnMongo/insertOpr.js
+++ curdOnMongo/insertOpr.js
@@ -1,6 +1,6 @@
 const conndb=require('../mongodb');
 // double .. use when import file outside of the current folder.
-
+ 
 const insert=async()=>{
     const db= await conndb();
     const data=await db.insertMany([{name:"Note6" , brand:"mi",price:"2600"},{name:"Note5" , brand:"mi",price:"2500"}])
